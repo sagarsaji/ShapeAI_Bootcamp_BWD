c10bfe15745f3a6a3a0b4a2d53341cf90f09b40d
Sagar Saji <61262546+sagarsaji@users.noreply.github.com>
2021-06-26 21:39:54 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>My Bio</h1>
+      <p>
+        I'm pursuing B.Tech in Computer Science at ASIET Kalady Kerala. I design
+        websites and also have some knowledge in coding. In my free hours I make
+        memes. Twinkle Twinkle little star, Raja's team is Triple Strong.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
